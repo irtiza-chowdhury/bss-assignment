74e91477455d453ee0730dd1dd3f153c2c056393
irtiza-chowdhury <irtizachowdhury5@gmail.com>
2023-06-12 13:36:15 +0600

    first commit

---
+++ jquery/jquery_js/custom.js
@@ -0,0 +1,20 @@
+$('#Carousel').owlCarousel({
+    loop:false,
+    stagePadding: 0,
+    margin:10,
+    dots:false,
+    nav:true,
+    slideBy:1,
+    responsive:{
+        0:{
+            items:2
+        },
+        
+        992:{
+            items:3
+        }
+    }
+})
+
+
+
